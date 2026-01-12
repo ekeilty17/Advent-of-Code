deae78fd8b152b8224c334dd5f839bee6820a05a
Eric Keilty <eric.keilty@ibm.com>
2026-01-11 23:07:22 -0500

    day 19 complete

--- 2015/Day_19/README.md
+++ 2015/Day_19/README.md
@@ -1,8 +1,15 @@
-# Day XX: ??
+# Day 19: Medicine for Rudolph
 
-[Problem Link](https://adventofcode.com/2015/day/XX)
+[Problem Link](https://adventofcode.com/2015/day/19)
 
 ## Part 1
 
+First, I wrote the function `parse_elements()` which converts the molecule string into its list of elements, for example `parse_elements("PTiMg") --> ["P", "Ti", "Mg"]`. Now, the goal to get all possible distinct elements that we can fabricate by a single replacement. To do this, I just iterate over this element list and do every possible replacement at each index. For example, the first element `P` can be replaced by `CaP`, `PTi`, or `SiRnFAr`. Therefore, just replacing that index results in the following molecules `["CaPTiMg", "PTiTiMg", "SiRnFArTiMg"]`. Each of the fabricated molecules are stored in a set to remove duplicates.
+
 ## Part 2 
 
+This problem relies on a cooperative input. Doing a complete brute force search takes way too long as the search space is way too large. It turns out, if you take the greedy approach and just replace the largest molecules first, you arrive at the optimal solution. This is not true in general and it's easy to come up with counter examples. 
+
+First, I do my search backwards. Instead of starting from `e` and additively replace to create the target molecule, I start at the target molecule and subtractively replace to get back to `e`. This reverse mapping is the dictionary `elements_by_replacement`.
+
+Then, my code implements a [Depth-First Search](https://en.wikipedia.org/wiki/Depth-first_search) where we traverse the largest replacements first. Then I just return the first time that I hit `e` and assume it's optimal. I don't love it, but it solves the problem I guess.
--- 2015/Day_19/part_2.py
+++ 2015/Day_19/part_2.py
@@ -1,63 +1,37 @@
 from pathlib import Path
-from typing import List, Dict, Set
+from typing import List, Dict, Optional
 
 from Day_19.const import DAY, INPUT_FILE_NAME
 from Day_19.const import EXAMPLE_INPUT_FILE_NAME_PART_2 as EXAMPLE_INPUT_FILE_NAME
 from Day_19.load_input import load_input
-from Day_19.part_1 import parse_elements, get_all_fabricatable_molecules
 from utils.solve import test, solve
 
-def get_all_substring_replacements(string: str, substring: str, replacement: str) -> Set[str]:
-    replacement_strings = set([])
-    n = len(substring)
-    for i in range(len(string) - n+1):
-        if string[i: i+n] == substring:
-            replacement_strings.add( string[:i] + replacement + string[i+n:] )
-    return replacement_strings
-
 def solution(replacements_by_element: Dict[str, List[str]], medicine_molecule: List[str]) -> int:
-    pass
-    """
     terminal_elements = replacements_by_element["e"]
     elements_by_replacement = {
         new_element: element 
         for element, replacements in replacements_by_element.items() for new_element in replacements
         if element != "e"
     }
-    print(terminal_elements)
-    print(elements_by_replacement)
-    print()
-    layer = [medicine_molecule]
-    step = 0
-    while not any(molecule in terminal_elements for molecule in layer):
-        new_layer = set([])
-        for molecule in layer:
-            for new_element, element in elements_by_replacement.items():
-                new_layer.update(get_all_substring_replacements(molecule, new_element, element))
-        layer = list(new_layer)
-        print(len(layer), layer[0])
-        step += 1
+
+    def dfs(elements_by_replacement: Dict[str, str], molecule: str, steps: int=0) -> Optional[int]:
+        
+        if molecule in terminal_elements:
+            return steps+1
+
+        sorted_element_replacements = sorted(elements_by_replacement.items(), key=lambda t: len(t[0]), reverse=True)
+        for new_element, element in sorted_element_replacements:
+            new_molecule = molecule.replace(new_element, element, 1)
+            if new_molecule == molecule:
+                continue
+            dfs_result = dfs(elements_by_replacement, new_molecule, steps+1)
+            if dfs_result:
+                return dfs_result
+        
+        return None
+
+    return dfs(elements_by_replacement, medicine_molecule)
     
-    return step + 1
-    """
-
-    """
-    layer = ["e"]
-    all_previous_molecules = set([])
-
-    steps = 0
-    while medicine_molecule not in layer:
-        new_layer = set([])
-        for molecule in layer:
-            new_layer.update(get_all_fabricatable_molecules(replacements_by_element, molecule))
-        layer = [molecule for molecule in new_layer if len(molecule) <= len(medicine_molecule) and molecule not in all_previous_molecules]
-        all_previous_molecules.update(layer)
-        print(len(layer), len(layer[0]), layer[0])
-        steps += 1
-    
-    return steps
-    """
-
 if __name__ == "__main__":
     example_replacements_by_element, example_medicine_molecule = load_input(Path(f"Day_{DAY:02d}/{EXAMPLE_INPUT_FILE_NAME}"))
     expected_answer = 6
