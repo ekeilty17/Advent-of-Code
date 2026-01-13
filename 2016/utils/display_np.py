6fab74f62805251c74a3d9dc9b79d7b43a669cc7
Eric Keilty <eric.keilty@ibm.com>
2026-01-13 00:45:13 -0500

    modify utils

--- 2016/utils/display_np.py
+++ 2016/utils/display_np.py
@@ -1,17 +1,18 @@
 import numpy as np
 from numpy.typing import NDArray
 
-def display_np_str(arr: NDArray[str]) -> str:
+def display_np_str(arr: NDArray[str], supress: bool=False) -> str:
     out = []
     for row in arr:
         out.append("".join(row))
     out_str = "\n".join(out)
-    print(out_str)
+    if not supress:
+        print(out_str)
     return out_str
 
-def display_np_int(arr: NDArray[int]) -> str:
-    return display_np_str(arr.copy().astype(str))
+def display_np_int(arr: NDArray[int], supress: bool=False) -> str:
+    return display_np_str(arr.copy().astype(str), supress)
 
-def display_np_bool(arr: NDArray[bool], true_str: str="X", false_str: str=".") -> str:
+def display_np_bool(arr: NDArray[bool], true_str: str="#", false_str: str=".", supress: bool=False) -> str:
     arr = np.where(arr.copy(), true_str, false_str)
-    return display_np_str(arr)
+    return display_np_str(arr, supress)
